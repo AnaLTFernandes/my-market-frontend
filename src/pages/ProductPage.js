ca35dcc587a1d60f576d84cd2d7ec46dd728e9c9
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-04-04 17:26:16 -0300

    feat: return to main screen from product description screen

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 import { ProductDetails } from "../components/products/ProductDetails";
 
 export function ProductPage({ cart }) {
 	return (
 		<main>
+			<Link to="/" className="return-main-link">
+				voltar
+			</Link>
+
 			<ProductDetails cart={cart} />
 		</main>
 	);
